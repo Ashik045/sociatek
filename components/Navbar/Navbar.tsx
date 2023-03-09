b98a06ade3c0847780cc2d4ce8280929103f9a66
Ashik045 <ashikurislam045@gmail.com>
2023-03-09 13:36:12 +0600

    navbar done

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,65 +1,142 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { FaSearch, FaTimes } from "react-icons/fa";
+import noPhoto from "../../images/no-photo.png";
+import styles from "./navbar.module.scss";
+
 const Navbar = () => {
+  const [toggler, setToggler] = useState(false);
+  const [user, setuser] = useState(false);
+  const [inpVal, setInpVal] = useState("");
+  const [APIData, setAPIData] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  // const { user, dispatch } = useContext(Context);
+  // const { input, dispatchh } = useContext(InpContext);
+  // const navigate = useNavigate();
+  const router = useRouter();
+
+  //   useEffect(() => {
+  //     axios.get(`https://weblog-backend.onrender.com/api/posts`).then((response) => {
+  //         setAPIData(response.data.message);
+  //     });
+  // }, []);
+
+  // navigate to login page
+  const handleClick = () => {
+    router.push("/login");
+    setToggler(false);
+  };
+
+  // const handleClick2 = () => {
+  //     navigate('/');
+  // };
+
+  // user logout function
+  // const logOut = () => {
+  //     // setUser(false);
+  //     setToggler(false);
+  //     dispatch({ type: 'LOGOUT' });
+  //     navigate('/');
+  // };
+
+  const handleChange = (e) => {
+    setInpVal(e.target.value);
+  };
+
+  // search and get data
+  const handleSubmit = (e) => {
+    //   e.preventDefault();
+    //   setInpVal(e.target.value);
+    //   if (inpVal?.length > 0) {
+    //       navigate('/');
+    //       dispatchh({ type: 'SEARCH_START' });
+    //       // eslint-disable-next-line no-unused-vars
+    //       const filterData = APIData.filter((item) =>
+    //           Object.values(item.title).join('').toLowerCase().includes(inpVal.toLowerCase())
+    //       );
+    //       setFilteredResults(filterData);
+    //       dispatchh({ type: 'SEARCH_END', payload: inpVal });
+    //       // console.log(filterData);
+    //   } else {
+    //       setFilteredResults(APIData);
+    //       dispatchh({ type: 'SEARCH_CLEAR' });
+    //   }
+  };
+
+  const handleClose = () => {
+    setInpVal("");
+    //   dispatchh({ type: 'SEARCH_CLEAR' });
+  };
+
   return (
-    <div className="navbar">
-      <div className="navbar_main">
-        <div className="nav_brand">
+    <div className={styles.navbar}>
+      <div className={styles.navbar_main}>
+        <div className={styles.nav_brand}>
           <h5>
             WE<span style={{ color: "#FB2576" }}>BLOG</span>
           </h5>
         </div>
 
-        <div className="nav_right">
-          <div className="search">
-            {/* <form onSubmit={handleSubmit}>
-                            <input
-                                type="text"
-                                placeholder="Search.."
-                                value={inpVal}
-                                onChange={handleChange}
-                            />
-                            {inpVal?.length !== 0 && (
-                                <FaTimes className="search_icon_close" onClick={handleClose} />
-                            )}
-                            <button type="submit">
-                                <FaSearch className="search_icon" />
-                            </button>
-                        </form> */}
+        <div className={styles.nav_right}>
+          <div className={styles.search}>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Search.."
+                value={inpVal}
+                onChange={handleChange}
+              />
+              {inpVal?.length !== 0 && (
+                <FaTimes
+                  className={styles.search_icon_close}
+                  onClick={handleClose}
+                />
+              )}
+              <button type="submit">
+                <FaSearch className={styles.search_icon} />
+              </button>
+            </form>
           </div>
 
-          {/* <div className="nav_menu">
-                        <NavLink to="/">
-                            <a href="">Home</a>
-                        </NavLink>
-                        <NavLink to="/seetings">
-                            <a href="">Profile</a>
-                        </NavLink>
-                        <NavLink to="/write">
-                            <a href="">Post</a>
-                        </NavLink>
-                        {user && (
-                            <NavLink to={`users/?user=${user.username}`}>
+          <div className={styles.nav_menu}>
+            <Link href="/" style={{ textDecoration: "none" }}>
+              <p>Home</p>
+            </Link>
+            <Link href="/seetings" style={{ textDecoration: "none" }}>
+              <p>Profile</p>
+            </Link>
+            <Link href="/write" style={{ textDecoration: "none" }}>
+              <p>Post</p>
+            </Link>
+            {/* {user && (
+                            <Link href={`users/?user=${user.username}`}>
                                 <a href="">My Posts</a>
-                            </NavLink>
-                        )}
-                    </div> */}
+                            </Link>
+                        )} */}
+            <Link href={`users/`} style={{ textDecoration: "none" }}>
+              <p>My Posts</p>
+            </Link>
+          </div>
 
           {/* nav reg sec */}
-          {/* <div className="nav_reg">
-                        {user ? (
-                            <>
-                                <img
-                                    onClick={() => navigate('/seetings')}
-                                    className="profilePic"
-                                    src={user.profilepic ? user.profilepic : noPhoto}
-                                    alt=""
-                                />
-                            </>
-                        ) : (
-                            <button type="button" onClick={handleClick}>
-                                Log in
-                            </button>
-                        )}
-                    </div> */}
+          <div className={styles.nav_reg}>
+            {user ? (
+              <>
+                <Image
+                  onClick={() => router.push("/seetings")}
+                  className="profilePic"
+                  src={noPhoto}
+                  alt="user profile"
+                />
+              </>
+            ) : (
+              <button type="button" onClick={handleClick}>
+                Log in
+              </button>
+            )}
+          </div>
 
           {/* for responsive device */}
           <div className="res_navbar">
