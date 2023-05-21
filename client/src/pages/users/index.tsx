0ca5a476450aecca99e9f978c8ce8e7d4079a709
Ashik045 <ashikurislam045@gmail.com>
2023-05-21 14:42:10 +0600

    users page complete. time to upload  for testing

--- client/src/pages/users/index.tsx
+++ client/src/pages/users/index.tsx
@@ -1,8 +1,10 @@
 import { Context } from "Context/Context";
 import axios from "axios";
 import Navbar from "components/Navbar/Navbar";
+import UserDiv from "components/UserDiv/UserDiv";
 import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { User } from "types.global";
 import styles from "../../styles/users.module.scss";
@@ -13,18 +15,16 @@ interface HomePageProps {
 
 const Users: NextPage<HomePageProps> = ({ users }) => {
   const [followed, setFollowed] = useState(false);
-  const [userNav, setUserNav] = useState("");
-  const [userss, setUserss] = useState([]);
-
-  useEffect(() => {
-    const userIds = users.map((user) => {
-      return user._id;
-    });
-
-    setUserss(userIds);
-  }, [users]);
+  const [userNav, setUserNav] = useState("allusers");
+  const [userss, setUserss] = useState<User[]>([]);
+  const [loading, setLoading] = useState(false);
 
   const { user } = useContext(Context);
+  const router = useRouter();
+
+  useEffect(() => {
+    setUserss(users);
+  }, [users]);
 
   //  ***********  add a follow request to the user profile ***********
   const setFollow = () => {
@@ -34,24 +34,41 @@ const Users: NextPage<HomePageProps> = ({ users }) => {
   // reveive the nav data when user clicks on it
   const handleUserNav = async (nav: string) => {
     setUserNav(nav);
-    console.log(nav);
 
     if (nav === "followers") {
-      const response = await axios.get(
-        `http://localhost:4000/api/user/${user?._id}/followers`
-      );
-      const followers = await response.data.message;
-      setUserss(followers);
-      // setUserss(user?.followers || []);
+      setLoading(true);
+      if (!user) return router.push("/login");
+
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/api/user/${user?._id}/followers`
+        );
+        const followers = response.data.message;
+        setUserss(followers);
+      } catch (error) {
+        console.error("Error fetching followers:", error);
+        setUserss([]);
+      }
+      setLoading(false);
     } else if (nav === "followings") {
-      const response = await axios.get(
-        `http://localhost:4000/api/user/${user?._id}/followings`
-      );
-      const followings = await response.data.message;
-      setUserss(followings);
-      // setUserss(user?.following || []);
+      setLoading(true);
+      if (!user) return router.push("/login");
+
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/api/user/${user?._id}/followings`
+        );
+        const followings = response.data.message;
+        setUserss(followings);
+      } catch (error) {
+        console.error("Error fetching followings:", error);
+        setUserss([]);
+      }
+      setLoading(false);
     } else {
+      // Reset to all users if no specific nav is selected
       setUserss(users);
+      setLoading(false);
     }
   };
 
@@ -67,6 +84,7 @@ const Users: NextPage<HomePageProps> = ({ users }) => {
 
       <div className={styles.users_page_main}>
         <div className={styles.users_page_nav}>
+          {/* Render user navigation buttons */}
           <div className={styles.users_page_nav_main}>
             <p
               className={
@@ -101,9 +119,25 @@ const Users: NextPage<HomePageProps> = ({ users }) => {
           </div>
         </div>
 
-        {userss.map((user) => {
-          return <User user={user} key={user._id} />;
-        })}
+        {/* if there is no followers then show the message  */}
+        {!loading && userss.length === 0 && userNav === "followers" && (
+          <p style={{ textAlign: "center" }}>
+            Currently not being followed by anyone!
+          </p>
+        )}
+
+        {/* if there is no followings then show the message  */}
+        {!loading && userss.length === 0 && userNav === "followings" && (
+          <p style={{ textAlign: "center" }}>
+            Haven&apos;t followed anyone yet!
+          </p>
+        )}
+
+        {loading ? (
+          <span className={styles.loader}></span>
+        ) : (
+          <UserDiv users={userss} setFollow={setFollow} />
+        )}
       </div>
     </div>
   );
