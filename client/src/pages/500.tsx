651e3ac208a891ff4a05da49781244c2c9076eda
Ashik045 <ashikurislam045@gmail.com>
2023-09-06 16:48:34 +0600

    added a custom error message

---
+++ client/src/pages/500.tsx
@@ -0,0 +1,11 @@
+const TimeOut = () => {
+  return (
+    <div>
+      <h1>504 - Server Timeout</h1>
+      <p>This happend for the vercel free plan or something</p>
+      <h4>Try refreshing this page.</h4>
+    </div>
+  );
+};
+
+export default TimeOut;
---
+++ client/src/pages/504.tsx
@@ -0,0 +1,11 @@
+const CustomError504 = () => {
+  return (
+    <div>
+      <h1>504 - Server Timeout</h1>
+      <p>This happend for the vercel free plan or something</p>
+      <h4>Try refreshing this page.</h4>
+    </div>
+  );
+};
+
+export default CustomError504;
